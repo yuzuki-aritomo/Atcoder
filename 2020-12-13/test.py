8380e27b3a9b26b612fb2579b255e001712eb566
yuzuki-aritomo <yuzuki.aritomo@gmail.com>
2020-12-13 23:30:11 +0900

    test

---
+++ 2020-12-13/test.py
@@ -0,0 +1,14 @@
+
+
+
+if __name__ == "__main__":
+    i = 0
+    k = 0
+    while(True):
+        while(True):
+            if(i ==10):
+                break
+            print("i",i)
+            print("k",k)
+            i += 1
+        k += 1
